bc6a2b0e411aa622ea4fee7c12ae17a7ef78ed2e
Timm Friebe <thekid@thekid.de>
2022-05-15 14:49:04 +0200

    Add tests for session IDs

--- src/test/php/web/session/unittest/CookieBasedTest.class.php
+++ src/test/php/web/session/unittest/CookieBasedTest.class.php
@@ -8,4 +8,22 @@ class CookieBasedTest extends SessionsTest {
   /** @return web.session.Sessions */
   protected function fixture() { return new CookieBased(new Secret('tlw3/ELaLfu3kmpzQJ0MDCdRG2b8Le+X')); }
 
+  #[Test]
+  public function id_changes_when_registering_values() {
+    $session= $this->fixture()->create();
+    $id= $session->id();
+    $session->register('key', 'value');
+
+    $this->assertNotEquals($id, $session->id());
+  }
+
+  #[Test]
+  public function id_changes_when_removing_registered_values() {
+    $session= $this->fixture()->create();
+    $id= $session->id();
+    $session->register('key', 'value');
+    $session->remove('key');
+
+    $this->assertNotEquals($id, $session->id());
+  }
 }
